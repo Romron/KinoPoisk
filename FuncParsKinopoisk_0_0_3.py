47a571a61d12965e50cb0bfad7c8e877e95268d9
Romron <romron@ukr.net>
2020-07-29 17:03:34 +0300

    Сделал предварительные заготовки. На пробном тесте из 100 proxy ни одного рабочего!!!!

---
+++ FuncParsKinopoisk_0_0_3.py
@@ -0,0 +1,118 @@
+from bs4 import BeautifulSoup
+import requests
+import array 
+import requests.exceptions 
+import re 
+
+
+def requestsURLThroughProxy(url,
+							proxyIP = 0,
+							_timeout=2,
+							headers={'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+							):
+
+	# Добавить оброботку протоколов SOKS
+
+	if proxyIP:
+		http_proxy = "http://" + proxyIP
+		https_proxy = "https://" + proxyIP 
+		proxies = {"http": http_proxy,
+				   "https":https_proxy}
+	# Попытка подключения к URLу
+	try:
+		response = requests.get(url,headers=headers,proxies=proxies,timeout=_timeout)
+		response.encoding = 'utf-8'
+	# Вывод результата в случаи успеха:
+		print('\n\n\n\n' + response.text + '\n\n\n\n')
+		return response.text
+	# Оброботка исключний:
+	except Exception as err:
+		# Вывод результата в случаи	неудачи
+		print('        proxy is not work')
+		return False
+
+def parsLinksAllFilmsInYear(Page):
+	# отладочные строки:
+	# def parsLinksAllFilmsInYear():
+	
+	arrLinksAllFilmsInYear = []
+	
+	# print("  parsLinksAllFilmsInYear: yuor page is being processed")
+	
+	# Проверка на страницу капчи:
+	if pageCapcha(Page):
+		print('pageCapcha')
+		return False
+
+	# отладочные строки:
+	# html = open('page.html',encoding='utf-8').read()
+	# soup = BeautifulSoup(html, 'lxml')
+	# Рабочие строки:
+	soup = BeautifulSoup(Page, 'lxml')
+
+	# ---------------------  ПАРСИНГ ссылок  ----------------------
+	#	получить ID_kinopoisk 
+	a_kinopoiskS = soup.findAll('a', class_='selection-film-item-meta__link')
+
+	for a in a_kinopoiskS:
+		ID_kinopoisk = a.get('href')
+		# print(ID_kinopoisk)
+		#	сформировать ссылку на страницу фильма
+		LinkToPageFilm = 'https://www.kinopoisk.ru' + ID_kinopoisk
+		# print(LinkToPageFilm)
+
+		arrLinksAllFilmsInYear.append(LinkToPageFilm) 
+
+	# отладочные строки:
+	# print("ParsLinksAllFilmsInYear: worked OK")
+	for x in arrLinksAllFilmsInYear:
+		print(x)
+	
+
+
+	# Рабочие строки:
+	return arrLinksAllFilmsInYear
+
+
+
+def pageCapcha(Page):
+	
+	soup = BeautifulSoup(Page, 'lxml')
+	tegCapch = re.search('запросы, поступившие с вашего IP-адреса, похожи на автоматические', soup.text)
+	
+	if tegCapch == None:
+		return False
+	return True
+
+
+def countProxyList(counterProxyList,mounthProxyList=1):
+	
+	
+
+	if counterProxyList < mounthProxyList :
+		counterProxyList += 1
+	else:
+		counterProxyList = 1
+	
+	proxyList = 'proxyList_' + str(counterProxyList)
+	
+	print('countProxyList:  ' + proxyList)
+	return proxyList
+
+
+
+def parsDateFilms():
+	pass
+
+
+
+
+
+
+# если этот файл используеться как подключаемый модуль то выполняються объявленные ф-ции
+# если этот файл используеться "сам по себе" то выполняються строки после этого услдовия
+if __name__ == '__main__':		
+	# print("Этот файл должен использоваться как подключаемый модуль!")
+
+	parsLinksAllFilmsInYear()
+ 
--- Pars_Date_about_AllFilms 0.0.1.py
+++ Pars_Date_about_AllFilms 0.0.1.py
@@ -3,122 +3,69 @@ import os.path
 import json
 import time
 
-from proxylist import *
-from FuncParsKinopoisk_0_0_2 import *
-
-
-#  переменные которые мог бы задавать пользователь:
-amountYears = 20 		# количество лет для поиска, начиная с 2020
-searchDepth = 20		# глубина  поиска в том же году
-
-
-# переменные необходимые для работы программыи
-counterYears = 0
-arrAllFilmsInYear = []
-arrLinksAllFilms = []
-
-
-pathDir = os.path.dirname(os.path.abspath(__file__)) +  "/json"		# для хранения файла с результатами работы этой ф-ции
-if not os.path.exists(pathDir) :
-	os.mkdir(pathDir)
-time = time.strftime("%d-%m-%Y %H.%M.%S", time.localtime())
-fileName = pathDir + '/arrLinksAllFilms '+ time +' .json'
-
-
-
-# counterProxyList = 1
-# mounthProxyList = 3
-# proxyList = proxyList_0
-
-# URLPageFilmsInYear = "https://www.kinopoisk.ru/lists/navigator/2020-2020/?sort=popularity&quick_filters=foreign%2Cfilms&tab=all"
-#                       https://www.kinopoisk.ru/lists/navigator/2020-2020/?page=2&sort=year&tab=all
-#                       https://www.kinopoisk.ru/lists/navigator/2020-2020/?page=2&quick_filters=foreign%2Cfilms&tab=all
-
-print('Start at ' + time)
-while counterYears < amountYears:
-	
-	URLPageFilmsInYear = "https://www.kinopoisk.ru/lists/navigator/" + str(2020-counterYears) + "-" + str(2020-counterYears) + "/?sort=popularity&quick_filters=foreign%2Cfilms&tab=all"
-	PageAllFilmsInYear = requestsURLThroughProxy(URLPageFilmsInYear,proxyList)
-	
-	print(URLPageFilmsInYear)
-
-	if not PageAllFilmsInYear :
-		# proxyList = countProxyList(counterProxyList)
-		# print('proxyList = ' + proxyList)
-		countProxy = 0
-		continue
-	
-	if PageAllFilmsInYear:
-		arrAllFilmsInYear = parsLinksAllFilmsInYear(PageAllFilmsInYear)
-		if not arrAllFilmsInYear:
-			continue
-		# ---------------------  Реализуем глубину поиска фильмов того же года  -------------
-		counterDepth = 2
-		while counterDepth < searchDepth:
-			print('  ParsLinksAllFilms:  год  ' + str(2020-counterYears)  + '    обработано страниц:  ' + str(counterDepth-1))
-			URLNextPageFilmsInYear = "https://www.kinopoisk.ru/lists/navigator/" + str(2020-counterYears) + "-" + str(2020-counterYears) + "/?page=" + str(counterDepth) + "&sort=popularity&quick_filters=foreign%2Cfilms&tab=all"
-			NextPageFilmsInYear = requestsURLThroughProxy(URLNextPageFilmsInYear,proxyList)
-			
-
-
-			# # ------------- ОТЛАДКА  ------------
-			# print('\n\n')
-			# print(URLNextPageFilmsInYear)
-
-			# print(NextPageFilmsInYear)
-
-			# print('\n\n')
+import FuncParsKinopoisk_0_0_3  as FPK
 
 
 
 
-			if not NextPageFilmsInYear:			
-				# proxyList = countProxyList(counterProxyList)
-				countProxy = 0
-				continue
-			
-			arrNextFilmsInYear = parsLinksAllFilmsInYear(NextPageFilmsInYear)
-			if not arrNextFilmsInYear:
-				continue
-			
-			for x in arrNextFilmsInYear:
-				arrAllFilmsInYear.append(x)
-			counterDepth +=1
-	
-	else:
-		ERROR = "ParsLinksAllFilms:   The PageAllFilmsInYear is not available"
-		continue
-	
-	print('Обработка страниц ' + str(2020-counterYears) + ' года окончена \n' )
-	
-
-	if os.path.isfile(fileName):
-		writeMode = 'a'
-		print('Добавил строки в существующий файл\n')
-	else:
-		writeMode = 'w'
-		print('Создал файл для записи строк\n')
-
-	with open(fileName, writeMode, encoding = 'utf-8') as f:
-		json.dump(arrAllFilmsInYear, f, indent = 2, ensure_ascii = False)
 
 
 
-	arrLinksAllFilms.append(arrAllFilmsInYear)
 
-	counterYears += 1
+timeStart = time.strftime("%d-%m-%Y %H.%M.%S", time.localtime())
 
-#  Расчёт общего времини работы скрипта
-# timeFinish = time.strftime("%d-%m-%Y %H.%M.%S", time.localtime())
-# print('Start at ' + time)
-# print('Finish at ' + timeFinish)
+print('Start at  ' + timeStart)
 
-# Вывод результатов
-n = 0
-for x in arrLinksAllFilms:
-	for y in x:
-		n += 1
-		print(str(n)+'. ' + str(y))
+
+with open('json/arrLinksAllFilms 22-06-2020 09.11.09 .json') as file_handle:	# получаю ссылки из файла в список
+    list_LinksToFilm = json.load(file_handle)
+
+with open('Proxylist/proxylist 29-07-2020 08.54.06 .json') as file_handle:	# получаю прокси из файла в список
+    list_Proxy = json.load(file_handle)
+
+count_LinksToFilm = 1
+for Link_ToFilm in list_LinksToFilm:
+
+	print(str(count_LinksToFilm) + '. ' + Link_ToFilm)
+	count_proxyIP = 0
+	for proxyIP in list_Proxy:
+		print('    ' + str(count_proxyIP) + '. ' + str(proxyIP))
+
+		html = FPK.requestsURLThroughProxy(Link_ToFilm,proxyIP)
+
+		count_proxyIP += 1
+		if html:
+			break
+	count_LinksToFilm += 1
+	if count_LinksToFilm > 10:
+		break
+
+
+
+
+
+# --------------------- Закрыл на время тестов -----------------------
+	# pathDir = os.path.dirname(os.path.abspath(__file__)) +  "/json"		# для хранения файла с результатами работы этой ф-ции
+	# if not os.path.exists(pathDir) :
+	# 	os.mkdir(pathDir)
+	# fileName = pathDir + '/arr_DateAllFilms '+ timeStart + ' .json'
+	# if os.path.isfile(fileName):
+	# 	writeMode = 'a'
+	# 	print('Добавил строки в существующий файл\n')
+	# else:
+	# 	writeMode = 'w'
+	# 	print('Создал файл для записи строк\n')
+	# with open(fileName, writeMode, encoding = 'utf-8') as f:
+	# 	json.dump(arr_DateAllFilms, f, indent = 2, ensure_ascii = False)
+
+
+# Расчёт общего времини работы скрипта
+# Хочу разницу времён, но пока не получается
+timeFinish = time.strftime("%d-%m-%Y %H.%M.%S", time.localtime())
+print('Finish at ' + timeFinish)
+
+
+
 
 
 # Добавить автоматическое выключение комьпютера 

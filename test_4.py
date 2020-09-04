9a2ba9801974c2b30113bf60638d146b7fea24d4
Romron <romron@ukr.net>
2020-09-04 16:15:58 +0300

    Изучение и эксперементы часть 2

--- test_4.py
+++ test_4.py
@@ -1,25 +1,23 @@
+
+'''
+	открыть картинку
+	создать маску
+		подобрать параметры
+		убрать шум
+	преобразования по маске
+'''
+
 import cv2
+import numpy as np
+
+
+img = cv2.imread(r"IMG/posters/1060511_1.jpeg")
+hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
+
+# cv2.imshow('1',img)
+# cv2.imshow('2',hsv_img)
 
 
 
-
-
-
-
-
-
-def main():
-	structure_ArrImg = cv2.imread('image.jpg')
-	cv2.imshow('window_1',structure_ArrImg)
-	cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-	main()
+cv2.waitKey(0)
+cv2.destroyAllWindows()

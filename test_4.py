7e92405cef365f86f40b59fe6bc4f9d4b9dd0ccc
Romron <romron@ukr.net>
2020-09-08 16:53:57 +0300

    что то

--- test_4.py
+++ test_4.py
@@ -15,12 +15,7 @@ import random
 img = cv2.imread(r"IMG/posters/1060511_1.jpeg")
 
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-ret, thresh_gray = cv2.threshold(gray_img, 9, 255, cv2.THRESH_BINARY_INV)
-
-
-
-
-
+ret, thresh_gray = cv2.threshold(gray_img, 74, 255, cv2.THRESH_BINARY_INV)
 
 cv2.imshow('gray_img',gray_img)
 cv2.imshow('thresh_gray',thresh_gray)

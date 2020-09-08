c6598bd61e30013af7dd09bae3d2b7a41f3f1d55
Romron <romron@ukr.net>
2020-09-08 15:43:23 +0300

    что-то

--- test_4.py
+++ test_4.py
@@ -5,21 +5,25 @@
 		подобрать параметры
 		убрать шум
 	преобразования по маске
+		с помощью cv2.inpaint()
 '''
 
 import cv2
 import numpy as np
 import random
 
-
 img = cv2.imread(r"IMG/posters/1060511_1.jpeg")
-hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
 
-# cv2.imshow('1',img)
-# cv2.imshow('2',hsv_img)
-
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh_gray = cv2.threshold(gray_img, 9, 255, cv2.THRESH_BINARY_INV)
 
 
+
+
+
+
+cv2.imshow('gray_img',gray_img)
+cv2.imshow('thresh_gray',thresh_gray)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
